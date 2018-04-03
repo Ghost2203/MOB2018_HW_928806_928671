f3ada86232b5343f2d09d11dbf1a19ff0c6fb236
Ghost2203 <glen.j.wider@student.fh-kiel.de>
2018-04-03 12:31:04 +0200

    First

--- app/src/main/java/com/mob2018_hw_928806_928671/ActivityMain.java
+++ app/src/main/java/com/mob2018_hw_928806_928671/ActivityMain.java
@@ -1,13 +1,36 @@
 package com.mob2018_hw_928806_928671;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.content.Intent;
+import android.widget.EditText;
+import android.util.Log;
 
-public class ActivityMain extends AppCompatActivity {
+public class ActivityMain extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private static final String TAG1 = "TAG";
+    private static final String TAG2 = "TAG";
+
+    public void onClickToast(View v) {
+        Toast.makeText(getApplicationContext(),
+                "Benjamin Haberkorn (928806), Glen Wider (928671)", Toast.LENGTH_LONG).show();
+        Log.i(TAG1, "Toast Button gedrückt");
+    }
+
+    public void onClickIntent(View v) {
+        Intent i = new Intent(this, ActivityLabel.class);
+        final EditText main2Input = (EditText) findViewById(R.id.main2Input);
+        String userMessage = main2Input.getText().toString();
+        i.putExtra("main2Message", userMessage);
+        startActivity(i);
+        Log.i(TAG2, "Intent Button gedrückt. Text: " + userMessage);
     }
 }
